1cbe4985c03530085c5c6cdef3b3c7afe6710361
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-03 16:43:28 +0200

    projeito feito com sucesso

--- cal.js
+++ cal.js
@@ -12,7 +12,7 @@ const opracaoPendente = () => operador != undefined
 
 const calcular = () => {
     if (opracaoPendente()) {
-        const numeroAtual = parseFloat(display.textContent)
+        const numeroAtual = parseFloat(display.textContent.replace(",","."))
         novoNumero = true
 
         const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`)
@@ -32,10 +32,10 @@ const calcular = () => {
 
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
-        display.textContent = texto
+        display.textContent = texto.toLocaleString("BR")
         novoNumero = false
     } else {
-        display.textContent += texto
+        display.textContent += texto.toLocaleString("BR")
     }
 }
 
@@ -48,7 +48,7 @@ const selecionarOperador = (evento) => {
         calcular()
         novoNumero = true
         operador = evento.target.textContent
-        numeroAnterior = parseFloat(display.textContent)
+        numeroAnterior = parseFloat(display.textContent.replace(",","."))
     }
 }
 operadores.forEach(operador => operador.addEventListener("click", selecionarOperador))
@@ -71,7 +71,7 @@ const limparCalculo = () => {
 document.getElementById("limparCalculo").addEventListener("click", limparCalculo)
 
 const remmoverUltimoNumero = () => display.textContent = display.textContent.slice(0, -1)
-document.getElementById("backSpace").addEventListener("click", remmoverUltimoNumero)
+document.getElementById("backspace").addEventListener("click", remmoverUltimoNumero)
 
 const inverterSinal = () => {
     novoNumero = true
@@ -91,3 +91,35 @@ const inserirDecimal = () => {
     }
 }
 document.getElementById("decimal").addEventListener("click", inserirDecimal)
+
+const mapaTeclado = {
+    "0":"tecla0",
+    "1":"tecla1",
+    "2":"tecla2",
+    "3":"tecla3",
+    "4":"tecla4",
+    "5":"tecla5",
+    "6":"tecla6",
+    "7":"tecla7",
+    "8":"tecla8",
+    "9":"tecla9",
+    "/":"operadorDividir",
+    "*":"operadorMultiplicar",
+    "-":"operadorSubtrair",
+    "+":"operadorAdicionar",
+    "=":"igual",
+    "Enter":"igual",
+    "Backspace":"backspace",
+    "c":"limparDisplay",
+    "Escape":"limparCalculo",
+    ",":"decimal"
+}
+
+const mapearTeclado =(evento)=>{
+    const tecla = evento.key
+
+    const teclaPermitida = ()=> Object.keys(mapaTeclado).indexOf(tecla)!= -1
+   if(teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click()
+}
+
+document.addEventListener("keydown", mapearTeclado)
